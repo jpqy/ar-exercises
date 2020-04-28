506056a4c9767a84656c9246b22765f1b3d0fb15
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-28 00:38:35 -0400

    finish ex4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,8 +7,8 @@ puts "----------"
 #class Store < ApplicationRecord
 #end
 
-burnaby = Store.create(annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-richmond = Store.create(annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-gastown = Store.create(annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,16 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts "Men's stores:"
+@mens_stores.each {|store| puts "Name: #{store.name}, Revenue: #{store.annual_revenue}"}
+
+@small_womens_stores = Store.where(womens_apparel: true, annual_revenue: 0..1_000_000)
+
+puts "Women's stores:"
+@small_womens_stores.each {|store| puts "Name: #{store.name}, Revenue: #{store.annual_revenue}"}
